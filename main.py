b5e89e931ef67ad3a79a631e182d247ca883f3a1
David de Jesus Escorcia Gomez <descorcia@jamar.com>
2022-06-07 18:01:52 -0500

    created model user

--- main.py
+++ main.py
@@ -1,6 +1,42 @@
+# Python
+from datetime import date
+from typing import Optional
+from uuid import UUID
+
+# Pydantic
+from pydantic import BaseModel, EmailStr, Field
+
+# FastAPI
 from fastapi import FastAPI
 
 app = FastAPI()
+
+# --- Models
+class UserBase(BaseModel):
+    user_id: UUID = Field(...)
+    email: EmailStr = Field(...)
+
+class UserLogin(UserBase):
+    password: str = Field(
+        ...,
+        min_length=8
+    )
+
+class User(UserBase):
+    fist_name: str = Field(
+        ...,
+        min_length=1,
+        max_length=50
+    )
+    last_name: str = Field(
+        ...,
+        min_length=1,
+        max_length=50
+    )
+    birth_date: Optional[date] = Field(default=None)
+    
+class Tweet(BaseModel):
+    pass
 
 @app.get(path="/")
 def home():
